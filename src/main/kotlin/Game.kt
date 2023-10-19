5165231d4731d77a4ea7f1c295082a1de99fc6c0
Pushpak <pushpak.gosavi7@gmail.com>
2023-10-19 15:23:32 +0530

    Initial Commit

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,5 +1,45 @@
 class Game {
+    private val board = MutableList<Cell>(size = 9) { Cell.Empty }
+    private var status: Status = Status.Ideal
+    private lateinit var player: Player
 
+    fun start() {
+        println("----------------------------------")
+        println("|  Welcome to TIC-TAC-TOE Game!    |")
+        println("   Pick a number from 0-8  ")
+        println("----------------------------------")
+        getName()
+        while (status is Status.Running){
+            getCall()
+        }
+    }
+
+    private fun getName() {
+        print("Please enter your Name: ")
+        val name = readlnOrNull()
+        try {
+            require(name != null)
+            player = Player(name = name, symbol = 'X')
+            println("It's your move, $name")
+            printBoard()
+        } catch (e: Throwable) {
+            println("Invalid Name.")
+        }
+    }
+
+    private fun getCall(){
+
+    }
+
+    private fun printBoard(){
+        println()
+        println("-------------")
+        println("|  ${board[0].placeHolder}  ${board[1].placeHolder}  ${board[2].placeHolder}  |")
+        println("|  ${board[3].placeHolder}  ${board[4].placeHolder}  ${board[5].placeHolder}  |")
+        println("|  ${board[6].placeHolder}  ${board[7].placeHolder}  ${board[8].placeHolder}  |")
+        println("-------------")
+        println()
+    }
 }
 
 data class Player(
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,4 @@
 fun main() {
-    val objTicTack = TicTack()
-    objTicTack.takeName()
-    objTicTack.startGame()
-}
-
-class  TicTack{
-     lateinit var name:String
-     lateinit var array:Array<Int>
-    fun takeName(){
-        println("Plese Write the name :  ")
-        name = readln()
-        println("Player name is $name")
-    }
-    fun startGame(){
-        println("Let Start the game $name")
-    }
+    val game = Game()
+    game.start()
 }
